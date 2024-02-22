534d1d0c8008236de0387a819d839c5e6224d27a
Ina Elise <113548269+Inaelise@users.noreply.github.com>
2024-02-22 10:01:46 +0100

    Cleanup

--- js/index.mjs
+++ js/index.mjs
@@ -1,4 +1,3 @@
-/* import { showProductDetail } from "./showProductDetail.mjs"; */
 import { API_URL } from "./utils/api.mjs";
 import { upDateAmount } from "./utils/iconCartAmount.mjs";
 import { fetchProducts } from "./utils/fetchProducts.mjs";
@@ -44,50 +43,3 @@ async function main() {
 }
 
 main();
-
-/* import { showProductDetails } from "./showProductDetail.mjs";
-
-async function main() {
-  try {
-    const { data: items } = await fetchProducts();
-    showProductDetails(items);
-    displayProductList(items);
-    console.log(items);
-  } catch (error) {
-    console.log("Error fetching products:", error);
-  }
-}
-
-main();
-
-async function fetchProducts() {
-  try {
-    const response = await fetch("https://v2.api.noroff.dev/rainy-days");
-    if (!response.ok) {
-      throw new Error("Failed to fetch products");
-    }
-    return await response.json();
-  } catch (error) {
-    throw new Error(`Error: ${error.message}`);
-  }
-}
-
-function displayProductList(items) {
-  const displayContainer = document.getElementById("display-container");
-
-  items.forEach((item) => {
-    const productItem = document.createElement("a");
-    productItem.href = `/product.html?id=${item.id}`;
-    productItem.title = "Click to view details";
-    productItem.classList.add("product-item");
-    productItem.innerHTML = `
-      <img src="${item.image.url}">
-      <h2>${item.title}</h2>
-      <h3 class="product-price">NOK ${item.price}</h3>
-    `;
-    if (displayContainer) {
-      displayContainer.appendChild(productItem);
-    }
-  });
-}
- */
